bf9bcdc3e8549d82663dfab8288091ae5dc2a3f7
Tomáš Hladký <tofisko@gmail.com>
2021-11-23 10:58:22 +0100

    Newlines for formated time printing
    -Added missing newline characters for printing time difference

--- est_time.cpp
+++ est_time.cpp
@@ -13,11 +13,11 @@ void print_diff_time(time_t time_diff)
         printf("ETA: %ldm:%02lds\n", time_diff / 60, time_diff % 60);
     } else if (time_diff >= 3600 && time_diff < 86400) {
         // hours
-        printf("ETA: %ldh:%02ldm:%02lds", time_diff / 3600, time_diff % 3600 / 60,
+        printf("ETA: %ldh:%02ldm:%02lds\n", time_diff / 3600, time_diff % 3600 / 60,
                time_diff % 3600 % 60);
     } else {
         // days
-        printf("ETA: %ld days, %ldh:%02ldm:%02lds", time_diff / 86400,
+        printf("ETA: %ld days, %ldh:%02ldm:%02lds\n", time_diff / 86400,
                time_diff % 86400 / 3600, time_diff % 86400 % 3600 / 60,
                time_diff % 86400 % 3600 % 60);
     }
