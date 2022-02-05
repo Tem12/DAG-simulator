602dd732b67afc26bc5cb032e165f8da0e97df80
Tomáš Hladký <tofisko@gmail.com>
2022-02-05 14:39:01 +0100

    Added licenses
    -Added license terms

--- main.cpp
+++ main.cpp
@@ -1,3 +1,49 @@
+/**
+* @file main.cpp
+* @brief Main file for DAG simulator implementation
+* @author Tomas Hladky <xhladk15@stud.fit.vutbr.cz>
+* @author Martin Peresini <iperesini@fit.vut.cz>
+* @date 2021 - 2022
+*
+* Original @author Gavin Andresen <gavinandresen@gmail.com>
+*/
+/*
+* Copyright (C) BUT Security@FIT, 2021 - 2022
+*
+* LICENSE TERMS
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+* 3. Neither the name of the Company nor the names of its contributors
+*    may be used to endorse or promote products derived from this
+*    software without specific prior written permission.
+*
+* ALTERNATIVELY, provided that this notice is retained in full, this
+* product may be distributed under the terms of the GNU General Public
+* License (GPL) version 2 or later, in which case the provisions
+* of the GPL apply INSTEAD OF those given above.
+*
+* This software is provided ``as is'', and any express or implied
+* warranties, including, but not limited to, the implied warranties of
+* merchantability and fitness for a particular purpose are disclaimed.
+* In no event shall the company or contributors be liable for any
+* direct, indirect, incidental, special, exemplary, or consequential
+* damages (including, but not limited to, procurement of substitute
+* goods or services; loss of use, data, or profits; or business
+* interruption) however caused and on any theory of liability, whether
+* in contract, strict liability, or tort (including negligence or
+* otherwise) arising in any way out of the use of this software, even
+* if advised of the possibility of such damage.
+*
+*/
+
 #include "scheduler.h"
 
 #include <assert.h>
@@ -154,11 +200,6 @@ void mempool_update(boost::random::mt19937 &rng, std::vector<Miner *> &miners, C
 
     // every 2 second add new transactions
     double tNext = s.getSimTime() + tx_wait_secs;
-
-    // n_blocks * 10 min mean time * 60 seconds
-    // if (tNext < 100*600) {
-    // TODO : this must be edited automatically in code, if set to an extreme
-    // high, simulation will last much longer
 
     if (!end_simulation) {
         auto f = boost::bind(&mempool_update, rng, miners, boost::ref(s));
--- standard_miner.hpp
+++ standard_miner.hpp
@@ -1,6 +1,49 @@
-//
-// Standard miner. Relays and mines on longest chain it has seen.
-//
+/**
+* @file standard_miner.hpp
+* @brief Main file for DAG simulator implementation
+* @author Tomas Hladky <xhladk15@stud.fit.vutbr.cz>
+* @author Martin Peresini <iperesini@fit.vut.cz>
+* @date 2021 - 2022
+*
+* Original @author Gavin Andresen <gavinandresen@gmail.com>
+*/
+/*
+* Copyright (C) BUT Security@FIT, 2021 - 2022
+*
+* LICENSE TERMS
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+* 3. Neither the name of the Company nor the names of its contributors
+*    may be used to endorse or promote products derived from this
+*    software without specific prior written permission.
+*
+* ALTERNATIVELY, provided that this notice is retained in full, this
+* product may be distributed under the terms of the GNU General Public
+* License (GPL) version 2 or later, in which case the provisions
+* of the GPL apply INSTEAD OF those given above.
+*
+* This software is provided ``as is'', and any express or implied
+* warranties, including, but not limited to, the implied warranties of
+* merchantability and fitness for a particular purpose are disclaimed.
+* In no event shall the company or contributors be liable for any
+* direct, indirect, incidental, special, exemplary, or consequential
+* damages (including, but not limited to, procurement of substitute
+* goods or services; loss of use, data, or profits; or business
+* interruption) however caused and on any theory of liability, whether
+* in contract, strict liability, or tort (including negligence or
+* otherwise) arising in any way out of the use of this software, even
+* if advised of the possibility of such damage.
+*
+*/
+
 #ifndef STANDARD_MINER_H
 #define STANDARD_MINER_H
 
