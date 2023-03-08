bde0eb0e188d162aff6560d6bf56a9aaef985dd4
Tomáš Hladký <xhladk15@vutbr.cz>
2023-03-08 19:42:29 +0100

    Create topology - check possible connections

--- post-process/create_topology.py
+++ post-process/create_topology.py
@@ -11,6 +11,7 @@ import itertools
 import copy
 import math
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--honest', type=int, required=True,
@@ -19,6 +20,10 @@ def main():
                         help='Number of honest nodes')
     parser.add_argument('-d', '--node_degree', type=str, required=True,
                         help='Node degree data filepath')
+    parser.add_argument('-p', '--malicious_power', type=float, required=True,
+                        help='Malicious node power')
+    parser.add_argument('-n', '--name', type=str, required=True,
+                        help='Outputs name')
 
     args = parser.parse_args()
 
@@ -28,8 +33,9 @@ def main():
 
     df = pd.read_csv(args.node_degree)
 
-    node_degree_gen = stats.rv_discrete(name='node_degree', seed=12, values=(list(df['node-degree']), list(df['probabilities'])))
-    
+    node_degree_gen = stats.rv_discrete(name='node_degree', seed=12, values=(
+        list(df['node-degree']), list(df['probabilities'])))
+
     # prob = []
     # for _ in range(0, 100000):
     #     prob.append(node_degree_gen.rvs())
@@ -37,117 +43,173 @@ def main():
     # plt.show()
 
     total_node_count = args.honest + args.malicious
-    
+
     g = nx.Graph()
 
-    END_NODES_TEST = math.ceil(total_node_count - 1) # Number of nodes to test edge connection score (potential connection)
-    PATH_NODES_TEST = total_node_count * 3 # Number of nodes to create average hop count
+    total_edges = 0
+    edge_drop_counter = 0
+
+    # Number of nodes to test edge connection score (potential connection)
+    END_NODES_TEST = math.ceil(28)
+    PATH_NODES_TEST = total_node_count  # Number of nodes to create average hop count
+
+    MAX_DIFF_EDGES_TRY = 50
+    MAX_EDGES_COUNT_TRY = 30
 
     if END_NODES_TEST >= total_node_count:
         print('Nodes to test must smaller than all nodes in output topology')
         exit(1)
 
     node_peers = {}
-    
+
     # Add nodes
     for i in range(0, total_node_count):
         # Add node to graph struct
         g.add_node(i)
-
+        
         # Set each node number of peers
         edges = node_degree_gen.rvs()
+        total_edges += edges
         node_peers[i] = {'total': edges, 'free': edges}
+    
+        # Add edges
+        for i in tqdm(range(0, total_node_count)):
+            edges = []
 
-    # Add edges
-    for i in range(0, total_node_count):
-        print(f'Node {i}')
-        edges = node_peers[i]['free']
-
-        for e in range(0, edges):
-            # print(f'Edge {e}')
-
-            # existing_cliques = list(itertools.takewhile(lambda x: len(x) <= MAX_CLIQUE_SIZE, nx.enumerate_all_cliques(g)))
-            # ex_clique_sizes = [0] * (MAX_CLIQUE_SIZE - (MIN_CLIQUE_SIZE - 1))
-            # for c in existing_cliques:
-            #     if len(c) >= MIN_CLIQUE_SIZE and len(c) <= MAX_CLIQUE_SIZE:
-            #         ex_clique_sizes[len(c) - MIN_CLIQUE_SIZE] += 1
-
-            start_node = i
-            end_nodes = random.sample(range(0, total_node_count), END_NODES_TEST)
-            scores = {}
-            
-            test_nodes1 = [random.randrange(total_node_count) for _ in range(PATH_NODES_TEST)]
-            test_nodes2 = [random.randrange(total_node_count) for _ in range(PATH_NODES_TEST)]
-            
-            while start_node in end_nodes:
-                end_nodes = random.sample(range(0, total_node_count), END_NODES_TEST)
-
-            # Fill initial score
-            for n in end_nodes:
-                scores[n] = 0
-
-            for n in range(0, len(end_nodes)):
-                end_node = end_nodes[n]
-                newG = copy.deepcopy(g)
-                newG.add_edge(start_node, end_node)
-
-                hops = []
-                for t in range(0, len(test_nodes1)):
-                    while test_nodes1[t] == test_nodes2[t]:
-                        test_nodes2[t] = random.randint(0, total_node_count - 1)
-                    
-                    try:
-                        path = list(nx.shortest_path(newG, source=test_nodes1[t], target=test_nodes2[t], method='dijkstra'))
-                        hops.append(len(path))
-                    except:
-                        hops.append(0)
+            edges_count_try = 0
+            while edges_count_try < MAX_EDGES_COUNT_TRY:
                 
-                avg_hop_count = sum(hops) / len(hops)
-                scores[end_nodes[n]] = avg_hop_count
-
-                # clique_sizes = [0] * (MAX_CLIQUE_SIZE - (MIN_CLIQUE_SIZE - 1))
-                # for c in cliques:
-                #     if len(c) >= MIN_CLIQUE_SIZE and len(c) <= MAX_CLIQUE_SIZE:
-                #         clique_sizes[len(c) - MIN_CLIQUE_SIZE] += 1
-                
-                # for i in range(0, len(clique_sizes)):
-                #     scores[end_nodes[n]] += (clique_sizes[i] - ex_clique_sizes[i]) * 1
-                    
-            # print(scores)
-            sorted_score_keys = list(dict(sorted(scores.items(), key=lambda item: item[1])).keys())
-
-            while True:
-                if len(sorted_score_keys) == 0:
-                    print('Error: Ran out of nodes to connect. Possibly increase NODES_TEST variable.')
-                    break
-                
-                new_end_node = sorted_score_keys[random.randint(math.ceil((len(sorted_score_keys) - 1) / 2), 0 if len(sorted_score_keys) == 0 else len(sorted_score_keys) - 1)]
-
-                if node_peers[new_end_node]['free'] > 0:
-                    g.add_edge(start_node, new_end_node)
-                    node_peers[start_node]['free'] -= 1
-                    node_peers[new_end_node]['free'] -= 1
-                    break
+                # If it was not possible to find some node to connect, cut off remaining edges,
+                # however, this node can still receive connection from other node
+                if edges_count_try + 1 < MAX_EDGES_COUNT_TRY:
+                    edges = node_peers[i]['free']
                 else:
-                    sorted_score_keys.remove(new_end_node)
+                    edges = []
 
+                for e in range(0, edges):
+                    edge_added = 0
+
+                    while edge_added < MAX_DIFF_EDGES_TRY:
+                        start_node = i
+                        end_nodes = random.sample(
+                            range(0, total_node_count), END_NODES_TEST)
+                        scores = {}
+
+                        test_nodes1 = [random.randrange(total_node_count)
+                                       for _ in range(PATH_NODES_TEST)]
+                        test_nodes2 = [random.randrange(total_node_count)
+                                       for _ in range(PATH_NODES_TEST)]
+
+                        while start_node in end_nodes:
+                            end_nodes = random.sample(
+                                range(0, total_node_count), END_NODES_TEST)
+
+                        # Fill initial score
+                        for n in end_nodes:
+                            scores[n] = 0
+
+                        for n in range(0, len(end_nodes)):
+                            end_node = end_nodes[n]
+                            newG = copy.deepcopy(g)
+                            newG.add_edge(start_node, end_node)
+
+                            hops = []
+                            for t in range(0, len(test_nodes1)):
+                                while test_nodes1[t] == test_nodes2[t]:
+                                    test_nodes2[t] = random.randint(
+                                        0, total_node_count - 1)
+
+                                try:
+                                    path = list(nx.shortest_path(
+                                        newG, source=test_nodes1[t], target=test_nodes2[t], method='dijkstra'))
+                                    hops.append(len(path))
+                                except:
+                                    hops.append(0)
+
+                            avg_hop_count = sum(hops) / len(hops)
+                            scores[end_nodes[n]] = avg_hop_count
+
+                        # print(scores)
+                        sorted_score_keys = list(
+                            dict(sorted(scores.items(), key=lambda item: item[1])).keys())
+
+                        while True:
+                            if len(sorted_score_keys) == 0:
+                                edge_added += 1
+                                if edge_added == MAX_DIFF_EDGES_TRY:
+                                    edge_drop_counter += 1
+                                break
+                            
+                            new_end_node = sorted_score_keys[random.randint(math.ceil(
+                                (len(sorted_score_keys) - 1) / 2), 0 if len(sorted_score_keys) == 0 else len(sorted_score_keys) - 1)]
+
+                            if node_peers[new_end_node]['free'] > 0:
+                                g.add_edge(start_node, new_end_node)
+                                node_peers[start_node]['free'] -= 1
+                                node_peers[new_end_node]['free'] -= 1
+                                edge_added = 2 * MAX_DIFF_EDGES_TRY
+                                break
+                            else:
+                                sorted_score_keys.remove(new_end_node)
+                            
+                if edge_added == MAX_DIFF_EDGES_TRY:
+                    edges_count_try += 1
+
+    # Plot connection histogram
+    prob = []
+    for i in range(0, len(list(nx.nodes(g)))):
+        prob.append(len(list(nx.neighbors(g, i))))
+    plt.hist(prob, bins=57)
+    plt.savefig(f'hist_{args.name}.pdf')
+
+    fc = open(f'peers_{args.name}.cfg', 'w')
+    for prob_i in range(min(prob), max(prob) + 1):
+        fc.write(f'{prob_i}, {prob.count(prob_i)}\n')
+    fc.close()
+
+    # Plot graph network visualization
     # nx.draw_networkx(g)
     # ax = plt.gca()
     # plt.axis("off")
     # # plt.show()
     # plt.savefig('./vizualization.pdf')
-    nx.draw_networkx(g, node_size=0.05, font_size=0.05, width=0.01,
-                     alpha=0.5, node_color='#069AF390')
+    # nx.draw_networkx(g, node_size=0.05, font_size=0.05, width=0.01, alpha=0.5, node_color='#069AF390')
     # nx.draw(G, node_size=10)
 
-    ax = plt.gca()
-    plt.figure(1, figsize=(100000, 100000), dpi=600)
-    plt.axis("off")
-    plt.tight_layout()
-    plt.savefig('./vizualization.pdf')
+    # ax = plt.gca()
+    # plt.figure(1, figsize=(100000, 100000), dpi=600)
+    # plt.axis("off")
+    # plt.tight_layout()
+    # plt.savefig(f'vizualization_{args.name}.pdf')
 
-            
-            
+    # Generate configuration
+    f = open(f'{args.name}.cfg', 'w')
+
+    honest_haspower = (1 - args.malicious * args.malicious_power) / \
+        (total_node_count - args.malicious)
+    malicious_haspower = args.malicious_power
+    for i in range(0, args.honest):
+        f.write(f'miner={honest_haspower} honest\n')
+
+    for i in range(0, args.malicious):
+        f.write(f'miner={malicious_haspower} malicious\n')
+
+     # Print separator line
+    f.write('\n')
+
+    resolved_nodes = {}
+    for i in range(0, total_node_count):
+        for j in list(nx.neighbors(g, i)):
+            if j in resolved_nodes:
+                continue
+            else:
+                f.write(f'biconnect={i} {j} 500\n')
+        resolved_nodes[i] = True
+    f.close()
+
+    print(f'Totla edges: {total_edges}')
+    print(f'Edge drop count: {edge_drop_counter}')
+
 
 if __name__ == '__main__':
     main()
