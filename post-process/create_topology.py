df6394e72c449cfa85b0655e8723e7d6cc92e604
Tomáš Hladký <xhladk15@vutbr.cz>
2023-03-08 20:01:01 +0100

    Fixed create topology edge check

--- post-process/create_topology.py
+++ post-process/create_topology.py
@@ -53,8 +53,8 @@ def main():
     END_NODES_TEST = math.ceil(28)
     PATH_NODES_TEST = total_node_count  # Number of nodes to create average hop count
 
-    MAX_DIFF_EDGES_TRY = 50
-    MAX_EDGES_COUNT_TRY = 30
+    MAX_DIFF_EDGES_TRY = 30
+    MAX_EDGES_COUNT_TRY = 10
 
     if END_NODES_TEST >= total_node_count:
         print('Nodes to test must smaller than all nodes in output topology')
@@ -72,88 +72,89 @@ def main():
         total_edges += edges
         node_peers[i] = {'total': edges, 'free': edges}
     
-        # Add edges
-        for i in tqdm(range(0, total_node_count)):
-            edges = []
+    # Add edges
+    for i in tqdm(range(0, total_node_count)):
+        edges = 0
 
-            edges_count_try = 0
-            while edges_count_try < MAX_EDGES_COUNT_TRY:
+        edges_count_try = 0
+        while edges_count_try < MAX_EDGES_COUNT_TRY:
                 
-                # If it was not possible to find some node to connect, cut off remaining edges,
-                # however, this node can still receive connection from other node
-                if edges_count_try + 1 < MAX_EDGES_COUNT_TRY:
-                    edges = node_peers[i]['free']
-                else:
-                    edges = []
+            # If it was not possible to find some node to connect, cut off remaining edges,
+            # however, this node can still receive connection from other node
+            if edges_count_try + 1 < MAX_EDGES_COUNT_TRY:
+                edges = node_peers[i]['free']
+            else:
+                edge_drop_counter += node_peers[i]['free']
+                edges = 0
 
-                for e in range(0, edges):
-                    edge_added = 0
+            for e in range(0, edges):
+                edge_added = 0
 
-                    while edge_added < MAX_DIFF_EDGES_TRY:
-                        start_node = i
+                while edge_added < MAX_DIFF_EDGES_TRY:
+                    start_node = i
+                    end_nodes = random.sample(
+                        range(0, total_node_count), END_NODES_TEST)
+                    scores = {}
+
+                    test_nodes1 = [random.randrange(total_node_count)
+                                   for _ in range(PATH_NODES_TEST)]
+                    test_nodes2 = [random.randrange(total_node_count)
+                                   for _ in range(PATH_NODES_TEST)]
+
+                    while start_node in end_nodes:
                         end_nodes = random.sample(
                             range(0, total_node_count), END_NODES_TEST)
-                        scores = {}
 
-                        test_nodes1 = [random.randrange(total_node_count)
-                                       for _ in range(PATH_NODES_TEST)]
-                        test_nodes2 = [random.randrange(total_node_count)
-                                       for _ in range(PATH_NODES_TEST)]
+                    # Fill initial score
+                    for n in end_nodes:
+                        scores[n] = 0
 
-                        while start_node in end_nodes:
-                            end_nodes = random.sample(
-                                range(0, total_node_count), END_NODES_TEST)
+                    for n in range(0, len(end_nodes)):
+                        end_node = end_nodes[n]
+                        newG = copy.deepcopy(g)
+                        newG.add_edge(start_node, end_node)
 
-                        # Fill initial score
-                        for n in end_nodes:
-                            scores[n] = 0
+                        hops = []
+                        for t in range(0, len(test_nodes1)):
+                            while test_nodes1[t] == test_nodes2[t]:
+                                test_nodes2[t] = random.randint(
+                                    0, total_node_count - 1)
 
-                        for n in range(0, len(end_nodes)):
-                            end_node = end_nodes[n]
-                            newG = copy.deepcopy(g)
-                            newG.add_edge(start_node, end_node)
+                            try:
+                                path = list(nx.shortest_path(
+                                    newG, source=test_nodes1[t], target=test_nodes2[t], method='dijkstra'))
+                                hops.append(len(path))
+                            except:
+                                hops.append(0)
 
-                            hops = []
-                            for t in range(0, len(test_nodes1)):
-                                while test_nodes1[t] == test_nodes2[t]:
-                                    test_nodes2[t] = random.randint(
-                                        0, total_node_count - 1)
+                        avg_hop_count = sum(hops) / len(hops)
+                        scores[end_nodes[n]] = avg_hop_count
 
-                                try:
-                                    path = list(nx.shortest_path(
-                                        newG, source=test_nodes1[t], target=test_nodes2[t], method='dijkstra'))
-                                    hops.append(len(path))
-                                except:
-                                    hops.append(0)
+                    # print(scores)
+                    sorted_score_keys = list(
+                        dict(sorted(scores.items(), key=lambda item: item[1])).keys())
 
-                            avg_hop_count = sum(hops) / len(hops)
-                            scores[end_nodes[n]] = avg_hop_count
-
-                        # print(scores)
-                        sorted_score_keys = list(
-                            dict(sorted(scores.items(), key=lambda item: item[1])).keys())
-
-                        while True:
-                            if len(sorted_score_keys) == 0:
-                                edge_added += 1
-                                if edge_added == MAX_DIFF_EDGES_TRY:
-                                    edge_drop_counter += 1
-                                break
+                    while True:
+                        if len(sorted_score_keys) == 0:
+                            edge_added += 1
+                            break
                             
-                            new_end_node = sorted_score_keys[random.randint(math.ceil(
-                                (len(sorted_score_keys) - 1) / 2), 0 if len(sorted_score_keys) == 0 else len(sorted_score_keys) - 1)]
+                        new_end_node = sorted_score_keys[random.randint(math.ceil(
+                            (len(sorted_score_keys) - 1) / 2), 0 if len(sorted_score_keys) == 0 else len(sorted_score_keys) - 1)]
 
-                            if node_peers[new_end_node]['free'] > 0:
-                                g.add_edge(start_node, new_end_node)
-                                node_peers[start_node]['free'] -= 1
-                                node_peers[new_end_node]['free'] -= 1
-                                edge_added = 2 * MAX_DIFF_EDGES_TRY
-                                break
-                            else:
-                                sorted_score_keys.remove(new_end_node)
+                        if node_peers[new_end_node]['free'] > 0:
+                            g.add_edge(start_node, new_end_node)
+                            node_peers[start_node]['free'] -= 1
+                            node_peers[new_end_node]['free'] -= 1
+                            edge_added = 2 * MAX_DIFF_EDGES_TRY
+                            break
+                        else:
+                            sorted_score_keys.remove(new_end_node)
                             
-                if edge_added == MAX_DIFF_EDGES_TRY:
-                    edges_count_try += 1
+            if edge_added != MAX_DIFF_EDGES_TRY or edges == 0:
+                edges_count_try = 2 * MAX_EDGES_COUNT_TRY
+            else:
+                edges_count_try += 1
 
     # Plot connection histogram
     prob = []
@@ -208,7 +209,7 @@ def main():
     f.close()
 
     print(f'Totla edges: {total_edges}')
-    print(f'Edge drop count: {edge_drop_counter}')
+    print(f'Possible edge drop count: {edge_drop_counter}')
 
 
 if __name__ == '__main__':
